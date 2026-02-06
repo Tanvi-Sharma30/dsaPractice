6912cff07cc51362afd7097a061d3cf943b4a05d
Tanvi-Sharma30 <tanvisha309@gmail.com>
2026-02-06 19:48:50 +0530

    bfs

---
+++ graph/traversal.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+    vector<int> bfs(int v, vector<int> adj[]){
+        vector<int> vis(v, 0);
+        queue<int>q;
+        q.push(0);
+        vis[0]=1;
+        vector<int>bfs;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+
+            for(auto it: adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
