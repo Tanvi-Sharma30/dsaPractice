bdafba44f7a7dfd17044fdb9be1e41dcab4c6286
Tanvi-Sharma30 <tanvisha309@gmail.com>
2026-04-29 00:46:51 +0530

    bfs

---
+++ tree.cpp/bfs.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(): val(0),left(nullptr),right(nullptr){}
+    TreeNode(int x): val(x),left(nullptr),right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x),left(left),right(right){}
+};
+
+class Solution {
+    public:
+    vector<vector<int>> levelOrder(TreeNode *root){
+        vector<vector<int>> ans;
+        if(root==NULL) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> level;
+            for(int i=0; i<size; i++){
+                TreeNode *node = q.front();
+                q.pop();
+                if(node->left !=NULL) q.push(node->left);
+                if(node->right!=NULL) q.push(node->right);
+                level.push_back(node->val);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
+
+int main(){
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->right = new TreeNode(6);
+
+    Solution sol;
+    vector<vector<int>> result = sol.levelOrder(root);
+
+    // Print result
+    for(int i = 0; i < result.size(); i++){
+        for(int j = 0; j < result[i].size(); j++){
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
